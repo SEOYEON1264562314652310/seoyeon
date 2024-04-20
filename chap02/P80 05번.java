1b97981b10cdf033635f543627cf5da46c9c8a76
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-04-20 23:24:06 +0900

    Create P80 05번.java

---
+++ chap02/P80 05번.java
@@ -0,0 +1,9 @@
+public class P80 {
+	public static void main(String[] args) 
+   {
+		char c;
+		c = 'c'; 
+		System.out.print((int) 'A' - (int) 'a'); 
+		System.out.println((char) (c - 32)); 
+    }
+}
