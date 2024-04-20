cb2701f17883b0ecaa854edf11f32c2fe5786a8e
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-04-20 22:55:10 +0900

    Create P80 04번.java

---
+++ chap02/P80 04번.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class p80 {
+	public static void main(String[] args) {
+		int a;
+		Scanner in = new Scanner(System.in);
+		System.out.print("초 단위 정수를 입력하세요 : ");
+		a = in.nextInt;
+		System.out,print
+	}
+}
