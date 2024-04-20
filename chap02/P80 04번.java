a8940d99bd4de53c3a9f67fa7a7b6f62712c872e
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-04-20 23:01:19 +0900

    Update P80 04번.java

--- chap02/P80 04번.java
+++ chap02/P80 04번.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-public class p80 {
+public class P80{
 	public static void main(String[] args) {
-		int a;
+		int m;
 		Scanner in = new Scanner(System.in);
 		System.out.print("초 단위 정수를 입력하세요 : ");
-		a = in.nextInt;
-		System.out,print
+		m = in.nextInt();
+		System.out.println(m / 3600 + "시간 " + m / 60 + "분" + m % 60 + "초"); //초를 3600으로 나눈 몫이 시간, 60으로 나눈 몫이 분, 60으로 나누고 남은 나머지가 초
 	}
-}
+} 
