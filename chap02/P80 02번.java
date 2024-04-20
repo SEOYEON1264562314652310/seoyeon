4c9ea02db758c75cc7313abdb5e22fc34da2cbbb
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-04-20 22:43:46 +0900

    Create P80 02번.java

---
+++ chap02/P80 02번.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class p80 {
+	public static void main(String[] args) {
+		int a;
+		Scanner in = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 : ");
+		a = in.nextInt();
+		System.out.print("a의 제곱은");
+		System.out.print(a * a);
+	}
+}
