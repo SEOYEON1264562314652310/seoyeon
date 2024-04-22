43237985029f0400f98d4d5f28d6821755e5719e
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-04-22 15:20:46 +0900

    Update p158 01번.java

--- chap04/p158 01번.java
+++ chap04/p158 01번.java
@@ -1,20 +1,16 @@
-package chap04;
-
 class Printer {
-	int num0fPapers = 0;
-
+	int numOfPapers = 0;
 	public void print(int amount) {
-		num0fPapers -= amount;
+		numOfPapers -= amount;
 	}
 }
 
 public class PrinterTest {
 	public static void main(String[] args) {
-
 		Printer p = new Printer();
-		p.num0fPapers = 100;
+		p.numOfPapers = 100;
 		p.print(70);
-		System.out.println(p.num0fPapers);
+		System.out.println(p.numOfPapers);
 	}
 
 }
