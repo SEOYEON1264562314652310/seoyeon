f50607a83986f6f16e215b41f868f707cbf7fce1
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-06-10 01:00:25 +0900

    Create p.165 05번.java

---
+++ chap04/p.165 05번.java
@@ -0,0 +1,23 @@
+class Line{
+    private int 길이;
+
+    public Line(int 길이) {
+        this.길이 = 길이;
+    }
+
+    boolean isSameLine(Line n){
+        if (n.길이 == 길이)
+            return true;
+        return false;
+    }
+}
+
+public class LineTest {
+    public static void main(String[] args) {
+        Line a = new Line(1);
+        Line b = new Line(1);
+
+        System.out.println(a.isSameLine(b));
+        System.out.println(a==b);
+    }
+}
