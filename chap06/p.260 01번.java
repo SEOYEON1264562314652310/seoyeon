5ac7532d0d63d34aa0a9b24eca273f54608ba738
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-06-10 01:10:17 +0900

    Create p.260 01번.java

---
+++ chap06/p.260 01번.java
@@ -0,0 +1,35 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        ColoredCircle a = new ColoredCircle(10,"빨간색");
+
+        c.show();
+        a.show();
+    }
+}
+
+class Circle{
+    int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    void show(){
+        System.out.println("반지름이 " + radius + "인 원이다.");
+    }
+}
+
+class ColoredCircle extends Circle{
+    String color;
+
+    public ColoredCircle(int radius, String color) {
+        super(radius);
+        this.color = color;
+    }
+
+    @Override
+    void show() {
+        System.out.println("반지름이 " + radius + "인 " + color +  " 원이다.");
+    }
+}
