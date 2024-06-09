e0fb77bb923804ae34132febc77739589fa9f74e
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-06-10 00:41:21 +0900

    Create p.341 01번.java

---
+++ chap 08/p.341 01번.java
@@ -0,0 +1,18 @@
+public class CarTest {
+    public static void main(String[] args) {
+        Car myCar = new Car("그랜저");
+        Car yourCar = new Car("그랜저");
+        if (!myCar.equals(yourCar))
+            System.out.println("내 자동차는 [" + myCar.name + "], 너 자동차는 [" + yourCar.name + "]로 모델이 다르다.");
+        else
+            System.out.println("내 자동차는 [" + myCar.name + "], 너 자동차는 [" + yourCar.name + "]로 모델이 같다.");
+    }
+}
+
+class Car{
+    String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+}
