95cf495ad09741e22338ca8d848f09bba3ee50dd
SEOYEON1264562314652310 <164117690+SEOYEON1264562314652310@users.noreply.github.com>
2024-06-16 00:55:53 +0900

    Update p.469 01번.java

--- chap11/p.469 01번.java
+++ chap11/p.469 01번.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 
-public class ch11 {
-    public static void main(String[] args) {
-        ArrayList<String> lst = new ArrayList<String>();
+public class ArrayListEx1 {
 
-        lst.add("갈매기");
-        lst.add("나비");
-        lst.add("다람쥐");
-        lst.add("라마");
+	public static void main(String[] args) {
 
-        lst.forEach(s -> {
-            if (s.length() == 2) System.out.println(s);
-        });
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("갈매기");
+		a.add("나비");
+		a.add("다람쥐");
+		a.add("라마");
 
-    }
+		for (int i = 0; i < 4; i++) {
+			String n = a.get(i);
+			if (n.length() == 2) 
+				System.out.println(n);
+		}
+		for (String animal : a)
+			if (animal.length() == 2)
+				System.out.println(animal);
+	}
+
 }
